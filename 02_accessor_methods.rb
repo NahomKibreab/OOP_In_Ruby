fa7da9cae31d01da576cd960e5d387dc40c6debb
Nahom Mehanzel <nknahom@gmail.com>
2021-09-13 02:02:56 +0000

    Calling Methods with self

--- 02_accessor_methods.rb
+++ 02_accessor_methods.rb
@@ -1,17 +1,31 @@
 class GoodDog
-  attr_accessor :name
+  # if you want only getter method use attr_reader
+  # if you want only setter method use attr_writer
+  attr_accessor :name, :height, :weight
 
-  def initialize(name)
-    @name = name
+  def initialize(n, h, w)
+    @name = n
+    @height = height
+    @weight = w
   end
 
   def speak
-    "#{@name} says arf!"
+    "#{name} says arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall." 
   end
 end
 
-sparky = GoodDog.new("Sparky")
-puts sparky.speak
-puts sparky.name
-sparky.name = "Spartacus"
-puts sparky.name
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
+
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info    
